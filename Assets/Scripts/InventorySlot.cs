b1433ae5fd60c4f6a7975c090c1706595523960e
gassachek <gmansurov18@gmail.com>
2024-11-05 17:40:03 +0300

    Небольшие изменения

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class InventorySlot : MonoBehaviour
 {
-    [SerializeField]private Image icon;
-    [SerializeField]private TMP_Text itemName;
-    [SerializeField]private TMP_Text itemCount;
+    [SerializeField]private Image _icon;
+    [SerializeField]private TMP_Text _itemName;
+    [SerializeField]private TMP_Text _itemCount;
 
     // Sprite itemIcon, 
     public void SetSlot(string name, int count)
     {
         // icon.sprite = itemIcon;
-        itemName.text = name;
-        itemCount.text = count.ToString();
+        _itemName.text = name;
+        _itemCount.text = count.ToString();
     }
 }
