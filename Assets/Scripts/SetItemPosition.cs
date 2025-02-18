364d273df2f8c33bb05688f853097accf680185f
gassachek <gmansurov18@gmail.com>
2025-02-18 17:37:25 +0300

    Изменение размера у объекта

--- Assets/Scripts/SetItemPosition.cs
+++ Assets/Scripts/SetItemPosition.cs
@@ -3,18 +3,18 @@ using UnityEngine.EventSystems;
 
 public class SetItemPosition : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    private GameManager gameManager;
     private RectTransform _rectTransform;
     private CanvasGroup _canvasGroup;
     private Transform _originalParent;
     private Canvas _parentCanvas;
+    private Vector2 _originalSize;
 
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
         _canvasGroup = GetComponent<CanvasGroup>();
-        gameManager = FindFirstObjectByType<GameManager>();
         _parentCanvas = GetComponentInParent<Canvas>();
+        _originalSize = _rectTransform.sizeDelta;
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -33,30 +33,30 @@ public class SetItemPosition : MonoBehaviour, IPointerDownHandler, IBeginDragHan
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (gameManager != null)
-        {
-            Vector2 localPoint;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                _parentCanvas.transform as RectTransform, 
-                eventData.position, 
-                _parentCanvas.worldCamera, 
-                out localPoint
-            );
-            _rectTransform.position = _parentCanvas.transform.TransformPoint(localPoint);
-        }
-        else
-        {
-            Debug.LogError("GameManager is not initialized!");
-        }
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _parentCanvas.transform as RectTransform, 
+            eventData.position, 
+            _parentCanvas.worldCamera, 
+            out localPoint
+        );
+        _rectTransform.position = _parentCanvas.transform.TransformPoint(localPoint);
+        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         _canvasGroup.blocksRaycasts = true;
+        
+        _rectTransform.sizeDelta = _originalSize;
+
         if (transform.parent == _parentCanvas.transform)
         {
+           
             transform.SetParent(_originalParent, true);
-            _rectTransform.position = Vector2.zero;
+            _rectTransform.anchoredPosition = Vector2.zero;
+
+            _rectTransform.sizeDelta = _originalSize;
         }
     }
 }
