988dd3f1504c73ba0ebb70bc214de444f4f242b2
gassachek <gmansurov18@gmail.com>
2025-02-26 16:21:23 +0300

    Новая логика добавления

--- Assets/Scripts/View/BoilerView.cs
+++ Assets/Scripts/View/BoilerView.cs
@@ -1,6 +1,7 @@
 using MVP;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class BoilerView : View<BoilerPresenter>, IDropHandler
 {
@@ -9,15 +10,25 @@ public class BoilerView : View<BoilerPresenter>, IDropHandler
         if (eventData.pointerDrag != null)
         {
             eventData.pointerDrag.transform.SetParent(transform, true);
-            
+
             RectTransform droppedObject = eventData.pointerDrag.GetComponent<RectTransform>();
             droppedObject.anchoredPosition = Vector2.zero;
-        
-            string itemName = eventData.pointerDrag.name;
-            Presenter.Add(itemName);
             
+            TMP_Text itemNameText = eventData.pointerDrag.GetComponentInChildren<TMP_Text>();
+            
+            if (itemNameText == null)
+            {
+                Debug.Log("ItemName не найден, предмет не добавлен.");
+                return;
+            }
+
+            string itemName = itemNameText.text;
+            Debug.Log($"Добавляется предмет: {itemName}");
+
+            Presenter.Add(itemName);
         }
     }
+
 
     public void Boiling()
     {
