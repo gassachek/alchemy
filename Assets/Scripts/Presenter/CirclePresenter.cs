be207d19b5cf3816148ddbe8e44a2c0fbecf7997
gassachek <gmansurov18@gmail.com>
2025-03-25 00:10:08 +0300

    Логика кружков

---
+++ Assets/Scripts/Presenter/CirclePresenter.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace.Presenter
+{
+    public class CirclePresenter
+    {
+        
+    }
+}
---
+++ Assets/Scripts/View/CircleView.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace.View
+{
+    public class CircleView
+    {
+        
+    }
+}
